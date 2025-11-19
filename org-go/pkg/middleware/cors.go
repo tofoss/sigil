4085d038f55a5219b72ed53a5a28f34598fed2eb
Tormod Fossum <tormod.fossum@nav.no>
2025-11-19 22:06:56 +0100

    allow patching

--- org-go/pkg/middleware/cors.go
+++ org-go/pkg/middleware/cors.go
@@ -6,7 +6,7 @@ import (
 
 var corsOptions = cors.Options{
 	AllowedOrigins:   []string{"http://localhost:5173"},
-	AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+	AllowedMethods:   []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 	AllowedHeaders:   []string{"Authorization", "Content-Type", "X-XSRF-TOKEN"},
 	AllowCredentials: true,
 	MaxAge:           3600,
